caa02b26c09e5a04d9d7754e135f2bf77a51b31e
losor2002 <114603230+losor2002@users.noreply.github.com>
2023-09-13 11:07:38 +0200

    Project initialization

---
+++ src/main/java/it/unisa/darn/DarnApplication.java
@@ -0,0 +1,13 @@
+package it.unisa.darn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unisa/darn/DarnApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisa.darn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
