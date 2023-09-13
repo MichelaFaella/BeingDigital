e519fb341440f12783a40d4075e09250cf3b3c03
losor2002 <114603230+losor2002@users.noreply.github.com>
2023-09-13 13:06:29 +0200

    Checkstyle added

--- src/main/java/it/unisa/darn/DarnApplication.java
+++ src/main/java/it/unisa/darn/DarnApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DarnApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DarnApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DarnApplication.class, args);
+  }
 
 }
