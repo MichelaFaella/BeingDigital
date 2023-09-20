a8a88084834243d089efef55a126c4189af5b8d2
losor2002 <114603230+losor2002@users.noreply.github.com>
2023-09-20 12:56:41 +0200

    Risorse controller and service done

--- src/main/java/it/unisa/darn/application/control/metainfo/RisorseController.java
+++ src/main/java/it/unisa/darn/application/control/metainfo/RisorseController.java
@@ -1,15 +1,25 @@
 package it.unisa.darn.application.control.metainfo;
 
+import it.unisa.darn.application.service.metainfo.RisorseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/risorse")
+@RequestMapping("/admin/risorse")
 public class RisorseController {
 
+  @Autowired
+  private RisorseService risorseService;
+
   @GetMapping
-  public String get() {
+  public String get(Model model) {
+    model.addAttribute("lezioni", risorseService.getAllLezioni());
+    model.addAttribute("racconti", risorseService.getAllRacconti());
+    model.addAttribute("metainfo", risorseService.getAllMetaInfo());
+    model.addAttribute("giochi", risorseService.getAllGiochi());
     return "metainfo/risorse";
   }
 }
---
+++ src/main/java/it/unisa/darn/application/service/metainfo/RisorseService.java
@@ -0,0 +1,48 @@
+package it.unisa.darn.application.service.metainfo;
+
+import it.unisa.darn.storage.entity.Gioco;
+import it.unisa.darn.storage.entity.Lezione;
+import it.unisa.darn.storage.entity.MetaInfo;
+import it.unisa.darn.storage.entity.Racconto;
+import it.unisa.darn.storage.repository.GiocoRepository;
+import it.unisa.darn.storage.repository.LezioneRepository;
+import it.unisa.darn.storage.repository.MetaInfoRepository;
+import it.unisa.darn.storage.repository.RaccontoRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class RisorseService {
+
+  @Autowired
+  private MetaInfoRepository metaInfoRepository;
+
+  @Autowired
+  private LezioneRepository lezioneRepository;
+
+  @Autowired
+  private RaccontoRepository raccontoRepository;
+
+  @Autowired
+  private GiocoRepository giocoRepository;
+
+  public List<MetaInfo> getAllMetaInfo() {
+    return metaInfoRepository.findAll(Sort.by(Sort.Direction.ASC, "keyword"));
+  }
+
+  public List<Lezione> getAllLezioni() {
+    return lezioneRepository.findAll(Sort.by(Sort.Direction.ASC, "titolo"));
+  }
+
+  public List<Racconto> getAllRacconti() {
+    return raccontoRepository.findAll(Sort.by(Sort.Direction.ASC, "titolo"));
+  }
+
+  public List<Gioco> getAllGiochi() {
+    return giocoRepository.findAll(Sort.by(Sort.Direction.ASC, "nome"));
+  }
+}
