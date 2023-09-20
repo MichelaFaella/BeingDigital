6bc031e9f72691c6c59941a9165720170bdfb01a
losor2002 <114603230+losor2002@users.noreply.github.com>
2023-09-20 15:44:45 +0200

    Lezione controller and service done

---
+++ src/main/java/it/unisa/darn/application/control/metainfo/LezioneController.java
@@ -0,0 +1,32 @@
+package it.unisa.darn.application.control.metainfo;
+
+import it.unisa.darn.application.service.metainfo.LezioneService;
+import it.unisa.darn.storage.entity.Lezione;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+
+@Controller
+@RequestMapping("/auth/lezione")
+public class LezioneController {
+
+  @Autowired
+  private LezioneService lezioneService;
+
+  @GetMapping
+  public String get(@RequestParam Long id, Model model) {
+    Optional<Lezione> optional = lezioneService.getLezione(id);
+    if (optional.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+    }
+
+    model.addAttribute("argomento", optional.get());
+    return "metainfo/argomento";
+  }
+}
