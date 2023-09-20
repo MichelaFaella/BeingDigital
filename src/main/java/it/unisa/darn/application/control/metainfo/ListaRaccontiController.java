aaa2f664e608f645b9748773e7f2922594abd386
losor2002 <114603230+losor2002@users.noreply.github.com>
2023-09-20 13:11:57 +0200

    ListaRaccontiController done

--- src/main/java/it/unisa/darn/application/control/metainfo/ListaRaccontiController.java
+++ src/main/java/it/unisa/darn/application/control/metainfo/ListaRaccontiController.java
@@ -1,6 +1,9 @@
 package it.unisa.darn.application.control.metainfo;
 
+import it.unisa.darn.application.service.metainfo.RisorseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/listaRacconti")
 public class ListaRaccontiController {
 
+  @Autowired
+  private RisorseService risorseService;
+
   @GetMapping
-  public String get() {
+  public String get(Model model) {
+    model.addAttribute("racconti", risorseService.getAllRacconti());
     return "metainfo/listaRacconti";
   }
 }
