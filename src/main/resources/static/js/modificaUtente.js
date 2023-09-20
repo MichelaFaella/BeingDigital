fefa54750e3c49fcf3a37350fef69aea50a3b174
Michela Faella <108745604+MichelaFaella@users.noreply.github.com>
2023-09-20 16:10:36 +0200

    modificaUtente.html added

---
+++ src/main/resources/static/js/modificaUtente.js
@@ -0,0 +1,169 @@
+function showHide(password) {
+
+    let p_nuova = document.getElementById('p_nuova');
+    let p_vecchia = document.getElementById('p_vecchia');
+    let occhio_vecchio = document.getElementById('occhio_vecchio');
+    let occhio_nuovo = document.getElementById('occhio_nuovo');
+
+    if (password) {
+        if (p_vecchia.type == 'password') {
+            p_vecchia.setAttribute('type', 'text');
+            occhio_vecchio.src = 'hide.png';
+        } else {
+            p_vecchia.setAttribute('type', 'password');
+            occhio_vecchio.src = 'view.png';
+        }
+    }
+
+    if (!password) {
+        if (p_nuova.type == 'password') {
+            p_nuova.setAttribute('type', 'text');
+            occhio_nuovo.src = 'hide.png';
+        } else {
+            p_nuova.setAttribute('type', 'password');
+            occhio_nuovo.src = 'view.png';
+        }
+    }
+}
+
+function validation_name(input) {
+    console.log("nome")
+
+    var pattern = /^[A-Za-z]+$/;
+
+    if (input == null) {
+        toast()
+        return false
+    }
+
+    if (input.match(pattern)) {
+        return true;
+    } else {
+        toast("Il nome non rispetta la sintassi");
+        return false;
+    }
+}
+
+function validation_surname(input) {
+    console.log("cognome")
+    var pattern = /^[A-Za-z ,.']+$/i;
+
+    if (input == null) {
+        toast()
+        return false
+    }
+
+    if (input.match(pattern)) {
+        return true;
+    } else {
+        toast("Il cognome non rispetta la sintassi");
+        return false;
+    }
+}
+
+
+function validation_email(input) {
+
+    var pattern = /^[^ ]+@[^ ]+.[a-z]{2,3}$/;
+    console.log("email")
+    if (input == null) {
+        return false
+    }
+
+    if (input.match(pattern)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validation_password(input) {
+    console.log("password")
+    var pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\p{P})[A-Za-z\d\p{P}]{8,}$/u;
+
+    if (input == null) {
+        toast()
+        return false
+    }
+
+    if (input.match(pattern)) {
+        return true;
+    } else {
+        console.log("P sbagliata")
+        toast("La password deve contenere almeno 8 caratteri, di cui almeno una lettera maiuscola, almeno una minuscola, almeno un numero e almeno un carattere speciale")
+        return false;
+    }
+}
+
+function toast(txt) {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    x.innerHTML = txt;
+    setTimeout(function () {
+        x.className = x.className.replace("show", "");
+    }, 3000);
+}
+
+function validate(obj) {
+
+    var email = document.getElementById("email").value;
+    var txt_em = document.getElementById("email");
+    var nome = document.getElementById("nome").value;
+    var txt_n = document.getElementById("nome");
+    var cognome = document.getElementById("cognome").value;
+    var txt_c = document.getElementById("cognome");
+    var password_v = document.getElementById("p_vecchia").value;
+    var txt_pv = document.getElementById("p_vecchia");
+    var password_n = document.getElementById("p_nuova").value;
+    var txt_pn = document.getElementById("p_nuova");
+
+    if (validation_email(email) && validation_name(nome) && validation_surname(cognome) && validation_password(password_v) && validation_password(password_n) && validEmail) {
+        console.log("entra1");
+
+
+    } else {
+        console.log("entra2");
+        if (!validation_email(email))
+            txt_em.style.color = "#c80e00";
+        if (!validation_password(password_v))
+            txt_pv.style.color = "#c80e00";
+        if (!validation_password(password_n))
+            txt_pn.style.color = "#c80e00";
+        if (!validation_name(nome))
+            txt_n.style.color = "#c80e00";
+        if (!validation_surname(cognome))
+            txt_c.style.color = "#c80e00";
+
+        return false;
+    }
+
+    return false;
+}
+
+
+//ajax
+let validEmail = false;
+$('#RegEmail').on("change", function () {
+    validEmail = false;
+    console.log(this.value);
+
+    document.getElementById('email_v').va
+
+
+    if (!validation_email(this.value)) {
+        return;
+    }
+
+    $.post('/existsEmail', {email: this.value}, function (data) {
+        var txt_em = document.getElementById("RegEmail");
+        if (data.result == true) {
+            txt_em.style.color = "#c80e00";
+            toast("Email già esistente.")
+        } else {
+            validEmail = true;
+            txt_em.style.color = "#78c800";
+            toast("Email valida.")
+        }
+
+    });
+})
--- src/main/resources/static/js/risorse.js
+++ src/main/resources/static/js/risorse.js
@@ -1,15 +1,41 @@
 var lista = document.getElementById("list");
-var conf = document.getElementById("conf");
+var arg = document.getElementById("argomentoRimosso");
+var meta = document.getElementById("metainfoRimossa");
+var gioco = document.getElementById("giocoRimosso");
 
 
-function showConferma() {
-    console.log('wewe')
+function showConfermaArg(key) {
+    console.log("è stata chiamata l'argomento con titolo: " + key.id);
+    document.getElementById('meta').value = key.id;
     lista.style.display = "none";
-    console.log('wewe')
-    conf.style.display = "flex";
+    arg.style.display = "flex";
 }
 
-function hideConferma() {
+function hideConfermaArg() {
     lista.style.display = "flex";
-    conf.style.display = "none";
+    arg.style.display = "none";
+}
+
+function showConfermaMeta(key) {
+    console.log("è stata chiamata la meta-info con keyword: " + key.id);
+    document.getElementById('meta').value = key.id;
+    lista.style.display = "none";
+    meta.style.display = "flex";
+}
+
+function hideConfermaMeta() {
+    lista.style.display = "flex";
+    meta.style.display = "none";
+}
+
+function showConfermaGioco() {
+    console.log("è stata chiamata il gioco con nome: " + key.id);
+    document.getElementById('meta').value = key.id;
+    lista.style.display = "none";
+    gioco.style.display = "flex";
+}
+
+function hideConfermaGioco() {
+    lista.style.display = "flex";
+    gioco.style.display = "none";
 }
