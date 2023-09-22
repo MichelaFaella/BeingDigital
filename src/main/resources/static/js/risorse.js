7b12364820c683b492c45c42e6b6e8c170935c93
Michela Faella <108745604+MichelaFaella@users.noreply.github.com>
2023-09-22 10:54:46 +0200

    Test layout fixed

--- src/main/resources/static/js/risorse.js
+++ src/main/resources/static/js/risorse.js
@@ -2,11 +2,25 @@ var lista = document.getElementById("list");
 var arg = document.getElementById("argomentoRimosso");
 var meta = document.getElementById("mammeta");
 var gioco = document.getElementById("giocoRimosso");
+var dom = document.getElementById("domandaRimossa");
 
+function showConfermaDom(key) {
+    console.log("è stata chiamata la domanda con id: " + key.id);
+    document.getElementById('id_dom').value = key.id;
+    document.getElementById('meta').value = "metainfo";
+    lista.style.display = "none";
+    dom.style.display = "flex";
+}
+
+function hideConfermaDom() {
+    lista.style.display = "flex";
+    dom.style.display = "none";
+}
 
 function showConfermaArg(key) {
-    console.log("è stata chiamata l'argomento con keyword: " + key.id);
-    document.getElementById('meta').value = key.id;
+    console.log("è stata chiamata l'argomento con id: " + key.id);
+    document.getElementById('id_arg').value = key.id;
+    document.getElementById('arg').value = "argomento";
     lista.style.display = "none";
     arg.style.display = "flex";
 }
@@ -17,8 +31,9 @@ function hideConfermaArg() {
 }
 
 function showConfermaMeta(key) {
-    console.log("è stata chiamata la meta-info con keyword: " + key.id);
-    document.getElementById('meta').value = key.id;
+    console.log("è stata chiamata la meta-info con id: " + key.id);
+    document.getElementById('id_meta').value = key.id;
+    document.getElementById('meta').value = "metainfo";
     lista.style.display = "none";
     meta.style.display = "flex";
 }
@@ -29,8 +44,9 @@ function hideConfermaMeta() {
 }
 
 function showConfermaGioco() {
-    console.log("è stata chiamata il gioco con nome: " + key.id);
-    document.getElementById('meta').value = key.id;
+    console.log("è stata chiamata il gioco con id: " + key.id);
+    document.getElementById('id_gioco').value = key.id;
+    document.getElementById('gioco').value = "gioco";
     lista.style.display = "none";
     gioco.style.display = "flex";
 }
