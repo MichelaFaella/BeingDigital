0eab2e9ca7b0a5d2224f7834e869d1ccbde9ba3d
losor2002 <114603230+losor2002@users.noreply.github.com>
2023-09-27 19:01:48 +0200

    Arrays added

---
+++ src/main/java/it/unisa/darn/control/metainfo/util/Arrays.java
@@ -0,0 +1,15 @@
+package it.unisa.darn.control.metainfo.util;
+
+import java.util.Collections;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Arrays {
+
+  public <T> List<T> shuffle(T... args) {
+    List<T> result = java.util.Arrays.asList(args);
+    Collections.shuffle(result);
+    return result;
+  }
+}
