6673fbeaa2c2c257fb0ab2f11ad8d52200fd0b91
Michela Faella <108745604+MichelaFaella@users.noreply.github.com>
2023-10-03 11:41:41 +0200

    Header fixed

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,7 +1,18 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.getElementById("menu");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
