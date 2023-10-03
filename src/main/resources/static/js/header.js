e7b7d62a2992e12c24e0a06e6c3e5bcc8dea78cb
Michela Faella <108745604+MichelaFaella@users.noreply.github.com>
2023-10-03 16:42:13 +0200

    Header fixed

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -6,13 +6,3 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 })
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-}
